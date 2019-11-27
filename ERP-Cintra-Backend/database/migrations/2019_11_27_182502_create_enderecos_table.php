1529a4bcfff3defff64acb942df025fa0c388d34
GutoVillela <gustavopalavillela@gmail.com>
2019-11-27 15:50:49 -0300

    Criação de Models Cliente, Endereco e Telefone

---
+++ ERP-Cintra-Backend/database/migrations/2019_11_27_182502_create_enderecos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->unsignedBigInteger('cliente_id');
+            $table->string('cep');
+            $table->string('logradouro');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('uf');
+            $table->string('numero');
+            $table->string('complemento');
+            $table->primary('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
